143a96a16cd2e18adc83080329162fbfacef11e1
AchilleJ <achille.jouguelet@gmail.com>
2025-05-16 12:55:11 +0200

    add custom labels for taxonomy

--- entity/class-taxonomy.php
+++ entity/class-taxonomy.php
@@ -20,10 +20,12 @@ if (!class_exists('Taxonomy')) {
     {
         protected $id = ''; // Identifiant de la taxonomy
         protected $name = ''; // Nom de la taxonomy affiché dans l'admin
+        protected $isFeminin = false; // Masculin par défaut
         protected $parent = true; // Active le système de hiérarchie de la taxonomy
         protected $public = true; // Rend la taxonomie publique
         protected $postId = ''; // l'id du postType lié à la taxonomy
         protected $fields = [];
+
 
         public function getId()
         {
@@ -44,6 +46,18 @@ if (!class_exists('Taxonomy')) {
         public function setName($name)
         {
             $this->name = $name;
+            return $this;
+        }
+
+
+        public function isFeminin(): bool
+        {
+            return $this->isFeminin;
+        }
+
+        public function setIsFeminin(bool $isFeminin): self
+        {
+            $this->isFeminin = $isFeminin;
             return $this;
         }
 
@@ -91,13 +105,14 @@ if (!class_exists('Taxonomy')) {
             return $this;
         }
 
-        public function __construct($taxonomy, $postId, $settings = [])
+        public function __construct($taxonomy, $settings = [], $postId)
         {
             $defaults = [
                 'name' => $taxonomy,
+                'isFeminin' => false,
                 'parent' => true,
                 'public' => true,
-                'fields' => []
+                'fields' => [],
             ];
 
             // Fusionne les options par défaut avec celles passées
@@ -105,6 +120,7 @@ if (!class_exists('Taxonomy')) {
 
             $this->setId($taxonomy);
             $this->setName($settings['name']);
+            $this->setIsFeminin($settings['isFeminin']);
             $this->setPostId($postId);
             $this->setParent($settings['parent']);
             $this->setPublic($settings['public']);
@@ -117,23 +133,52 @@ if (!class_exists('Taxonomy')) {
 
         public function addTaxonomy()
         {
+            $feminin = $this->isFeminin();
+            $name_lower = strtolower($this->getName());
+
+            $labels = [
+                'name' => esc_html__($this->getName(), 'ovs'),
+                'singular_name' => esc_html__($this->getName(), 'ovs'),
+                'search_items' => esc_html__('Rechercher des ' . $name_lower, 'ovs'),
+                'all_items' => $feminin 
+                    ? esc_html__('Toutes les ' . $name_lower, 'ovs') 
+                    : esc_html__('Tous les ' . $name_lower, 'ovs'),
+                'parent_item' => $this->getParent() 
+                    ? esc_html__($name_lower . ' parent', 'ovs') 
+                    : null,
+                'edit_item' => $feminin
+                    ? esc_html__('Modifier la ' . $name_lower, 'ovs')
+                    : esc_html__('Modifier le ' . $name_lower, 'ovs'),
+                'update_item' => esc_html__('Mettre à jour', 'ovs'),
+                'add_new_item' => $feminin
+                    ? esc_html__('Ajouter une nouvelle ' . $name_lower, 'ovs')
+                    : esc_html__('Ajouter un nouveau ' . $name_lower, 'ovs'),
+                'new_item_name' => $feminin
+                    ? esc_html__('Nom de la nouvelle ' . $name_lower, 'ovs')
+                    : esc_html__('Nom du nouveau ' . $name_lower, 'ovs'),
+                'menu_name' => esc_html__($this->getName(), 'ovs')
+            ];
+        
+            $args = array(
+                'labels'            => $labels,
+                'hierarchical'     => $this->getParent(),
+                'public'           => $this->getPublic(),
+                'show_ui'          => true,
+                'show_admin_column'=> true,
+            );
+        
             register_taxonomy(
                 $this->getPostId() . '_' . $this->getId(),
                 $this->getPostId(),
-                array(
-                    'label' => esc_html__($this->getName(), 'ovs'),
-                    'hierarchical' => $this->getParent(),
-                    'public' => $this->getPublic(),
-                    'show_ui' => true
-                )
+                $args
             );
-
+        
             if (!empty($this->getFields())) {
                 $taxoId = $this->getPostId() . '_' . $this->getId();
                 $meta = new Meta_Taxonomy($taxoId, $this->getFields());
             }
         }
-
+        
         public function editTaxonomy($id, $name, $parent = true, $fields = [])
         {
             $this->setId($id);
