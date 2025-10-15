c0a64ac3958a74498a3f8f3167ee248e5a9d5f36
AchilleJ <achille.jouguelet@gmail.com>
2025-10-15 15:55:04 +0200

    update radio field

--- src/fields/radio.php
+++ src/fields/radio.php
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 if(!class_exists('Field_radio')) {
-    class Field_radio extends Field
+   class Field_radio extends Field
     {
         protected $options = [];
         /**
@@ -39,38 +39,52 @@ if(!class_exists('Field_radio')) {
         {
             parent::__construct($field, $value);
             $this->setOptions(array_key_exists('options', $field) ? $field['options'] : false);
+
+            // Valeur par défaut
+            if (empty($this->getValue()) && isset($field['default'])) {
+                $this->setValue($field['default']);
+            }
         }
+
         public function render()
         {
             $name = !empty($this->getName()) ? $this->getName() : $this->getId();
             $options = !empty($this->getOptions()) ? $this->getOptions() : false;
-            if(!empty($this->getId())) {
+
+            if (!empty($this->getId())) {
                 echo '<div class="form-row row-radio">';
-                if(!empty($this->getLabel())) {
-                    echo '<label for="' . esc_attr($this->getId()) . '">' . esc_attr($this->getLabel()) . '</label>';
+                if (!empty($this->getLabel())) {
+                    echo '<label>' . esc_html($this->getLabel()) . '</label>';
                 }
 
                 echo '<div class="choice-list">';
-                if($options) {
+                if ($options) {
                     $i = 1;
-                    foreach($options as $opt) {
-                        $v = str_replace(' ', '_', $opt);
-                        $v = $i . '_' . $this->getId() . '_' . preg_replace('/[^a-zA-Z0-9]/', '', $v);
+                    foreach ($options as $value => $label) {
+                        // Si l’array est numéroté (cas ['oui', 'non'])
+                        if (is_int($value)) {
+                            $value = $label;
+                        }
+
+                        $input_id = $this->getId() . '_' . $i;
+
                         echo '<div class="choice">';
-                        echo '<input type="radio" id="' . $v . '" name="' . $name . '" value="' . $v . '" ' . checked($this->getValue(), $v, false) . '>';
-                        echo '<label for="' . $v . '">' . $opt . '</label>';
+                        echo '<input type="radio" id="' . esc_attr($input_id) . '" name="' . esc_attr($name) . '" value="' . esc_attr($value) . '" ' . checked($this->getValue(), $value, false) . '>';
+                        echo '<label for="' . esc_attr($input_id) . '">' . esc_html($label) . '</label>';
                         echo '</div>';
+
                         $i++;
                     }
                 }
                 echo '</div>';
 
-                if(!empty($this->getSub_desc())) {
-                    echo '<p>' . esc_attr($this->getSub_desc()) . '</p>';
+                if (!empty($this->getSub_desc())) {
+                    echo '<p>' . esc_html($this->getSub_desc()) . '</p>';
                 }
+
                 echo '</div>';
             } else {
-                echo 'Erreur, l\'identifiant du champ est obligatoire. Vérifiez qu\'il ne soit pas vide.';
+                echo 'Erreur : l\'identifiant du champ est obligatoire.';
             }
         }
     }
